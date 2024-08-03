78684eb67e2f92ad441f19738e61a56b6b6427ba
aloneInTheCrowd <vlad.smelyanets@gmail.com>
2024-08-03 23:47:44 +0300

    cleanup

--- src/main/java/com/aloneinthecrowd/healthcheck/HealthController.java
+++ src/main/java/com/aloneinthecrowd/healthcheck/HealthController.java
@@ -3,12 +3,13 @@ package com.aloneinthecrowd.healthcheck;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/")
+@RestController
 public class HealthController {
 
-    public record Health(String status){}
+    public record Health(String status) {
+    }
 
-    @GetMapping("health")
+    @GetMapping("/health")
     public Health getHealth() {
         return new Health("OK");
     }
